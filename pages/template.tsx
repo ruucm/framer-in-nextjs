00a089dba4850c86687238ffd5a0ddaa5552c80a
ruucm <ruucm.a@gmail.com>
2021-09-13 16:55:50 +0900

    baseUrl

--- pages/template.tsx
+++ pages/template.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/dist/client/router"
 import dynamic from "next/dynamic"
 
 const getDynamicComponent = (c: any) =>
-  dynamic(() => import(`../components/${c}`), {
+  dynamic(() => import(`@/components/${c}`), {
     ssr: false,
     loading: () => <p>Loading...</p>,
   })
