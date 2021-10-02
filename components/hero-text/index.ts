bc213b1a05e79e9c867882624aedc9b16ceda2cd
ruucm <ruucm.a@gmail.com>
2021-10-02 11:58:39 +0900

    match official handshake guide & cleanups

--- components/hero-text/index.ts
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic"
-
-const DynamicComponentWithNoSSR = dynamic(() => import("./hero-text"), {
-  ssr: false,
-})
-
-export default DynamicComponentWithNoSSR
--- components/spinner/index.ts
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic"
-
-const DynamicComponentWithNoSSR = dynamic(() => import("./spinner"), {
-  ssr: false,
-})
-
-export default DynamicComponentWithNoSSR
